a1efc31ba11182ae756224d055311db3f8721171
bernardmendoza <bernardmendoza@outlook.ph>
2024-05-08 15:15:04 +0800

    L2-P1 ExecutionCache

--- src/level2/problem1/index.ts
+++ src/level2/problem1/index.ts
@@ -1,11 +1,21 @@
 export class ExecutionCache<TInputs extends Array<unknown>, TOutput> {
+  private cache: Map<string, Promise<TOutput>> = new Map();
+
   constructor(private readonly handler: (...args: TInputs) => Promise<TOutput>) {}
   
   async fire(key: string, ...args: TInputs): Promise<TOutput> {
-    /**
-     * insert your code here
-     */
-  
-    return;
+    // Check if the result is already cached
+    if (this.cache.has(key)) {
+
+      return this.cache.get(key)!; // Return cached result
+
+    } else {
+
+      // Execute the handler function and cache the result
+      const resultPromise = this.handler(...args);
+      this.cache.set(key, resultPromise);
+      return resultPromise;
+      
+    }
   }
 }
